0a546710e572e2542931483967666b2be0033047
kareemta <83985818+kareemta@users.noreply.github.com>
2024-04-24 16:19:08 -0400

    nav now works between all pages

--- app/src/main/java/com/example/actualfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/actualfinalproject/MainActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.URI
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WeatherFragment.OnFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
+
+
+    override fun onFragmentInteraction(uri: URI) {
+        TODO("Not yet implemented")
     }
 }
